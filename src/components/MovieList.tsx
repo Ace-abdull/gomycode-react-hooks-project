0505142f95c256ea80134bb45791b5908c47160c
Abdullahi Sudeis <abdullahisudeismohamud@gmail.com>
2026-05-27 15:42:08 +0300

    feat: pass movie click handler through MovieList

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -6,13 +6,15 @@ export interface Movie {
   description: string;
   posterURL: string;
   rating: number;
+  trailerURL: string;
 }
 
 interface MovieListProps {
   movies: Movie[];
+  onMovieClick: (movie: Movie) => void;
 }
 
-export default function MovieList({ movies }: MovieListProps) {
+export default function MovieList({ movies, onMovieClick }: MovieListProps) {
   if (movies.length === 0) {
     return (
       <p className="rounded-[1.75rem] bg-slate-950/80 p-8 text-center text-slate-400 ring-1 ring-white/10">
@@ -24,7 +26,7 @@ export default function MovieList({ movies }: MovieListProps) {
   return (
     <div className="mt-6 grid gap-6 sm:grid-cols-2 xl:grid-cols-3">
       {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} onClick={onMovieClick} />
       ))}
     </div>
   );
